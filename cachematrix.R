384d6b6f2f4653c21586c10834d24b9a4626223b
me19d049 <121347863+me19d049@users.noreply.github.com>
2022-12-24 19:47:35 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function stores old data for inverse and pass new matrix to calculate inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  minv=NULL
+  set=function(y)
+  {
+    x<<-y
+    minv<<-NULL
+  }
+  get=function()x
+  setinv=function(inv) minv<<-inv
+  getinv=function() minv
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## This function checks if matrix is same then do not calculate inverse and then it calculates inverse for new matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  minv=x$getinv()
+  if(!is.null(minv))
+  {
+    message("getting chached data")
+    return(minv)
+  }
+  data=x$get()
+  minv=solve(data)
+  x$setinv(minv)
+  minv
 }
